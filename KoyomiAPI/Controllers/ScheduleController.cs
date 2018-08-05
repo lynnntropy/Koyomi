a7895306567533eaee9080a65810d08394a32c43
Veselin Romić <omegavesko@gmail.com>
2018-08-05 18:31:25 +0200

    Add API project

---
+++ KoyomiAPI/Controllers/ScheduleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ScheduleFetcher;
+using ScheduleFetcher.Models;
+
+namespace KoyomiAPI.Controllers
+{
+    [Route("api")]
+    [Produces("application/json")]
+    public class ScheduleController : Controller
+    {
+        private readonly ScheduleService _scheduleService;
+
+        public ScheduleController()
+        {
+            _scheduleService = new ScheduleService();
+        }
+
+        [HttpGet("schedule")]
+        public async Task<List<ScheduleItem>> GetSchedule()
+        {
+            return await _scheduleService.GetScheduleItemsAsync();
+        }
+    }
+}
