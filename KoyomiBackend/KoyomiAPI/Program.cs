c26aea5198c431740351cfbe661a7d321997d7e1
Veselin Romić <omegavesko@gmail.com>
2018-08-12 23:49:32 +0200

    Changed API port to 5501

--- KoyomiBackend/KoyomiAPI/Program.cs
+++ KoyomiBackend/KoyomiAPI/Program.cs
@@ -20,6 +20,7 @@ namespace KoyomiAPI
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5501")
                 .Build();
     }
 }
