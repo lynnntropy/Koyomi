ede7b3f7b37a67dbba591ff7fb9d2aeab1a099ce
Veselin Romić <omegavesko@gmail.com>
2018-08-12 23:33:57 +0200

    Add simple in-memory cache to API

--- KoyomiBackend/KoyomiAPI/Controllers/ScheduleController.cs
+++ KoyomiBackend/KoyomiAPI/Controllers/ScheduleController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ScheduleFetcher;
@@ -11,6 +12,9 @@ namespace KoyomiAPI.Controllers
     public class ScheduleController : Controller
     {
         private readonly ScheduleService _scheduleService;
+        
+        private static List<ScheduleItem> _cachedSchedule;
+        private static DateTime _cacheTime;
 
         public ScheduleController()
         {
@@ -20,7 +24,14 @@ namespace KoyomiAPI.Controllers
         [HttpGet("schedule")]
         public async Task<List<ScheduleItem>> GetSchedule()
         {
-            return await _scheduleService.GetScheduleItemsAsync();
+            // Refresh the cache if it's nonexistent or stale (over an hour old)
+            if (_cachedSchedule == null || (DateTime.Now - _cacheTime).TotalHours >= 1)
+            {
+                _cachedSchedule = await _scheduleService.GetScheduleItemsAsync();
+                _cacheTime = DateTime.Now;
+            }
+
+            return _cachedSchedule;
         }
     }
 }
